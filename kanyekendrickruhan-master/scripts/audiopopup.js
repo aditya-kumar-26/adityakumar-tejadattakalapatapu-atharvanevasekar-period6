9b472bc6b4575e76e3fc533b73ea06272d285a36
aditya-kumar-26 <awesomeadi54@gmail.com>
2021-12-16 22:38:45 -0800

    js audio added

---
+++ kanyekendrickruhan-master/scripts/audiopopup.js
@@ -0,0 +1,45 @@
+// When the user clicks on <div>, open the popup
+function s80() {
+    var popup = document.getElementById("s80");
+    popup.classList.toggle("show");
+}
+
+function gkmc() {
+    var popup = document.getElementById("gkmc");
+    popup.classList.toggle("show");
+}
+
+function tpab() {
+    var popup = document.getElementById("tpab");
+    popup.classList.toggle("show");
+}
+
+function damn() {
+    var popup = document.getElementById("damn");
+    popup.classList.toggle("show");
+}
+
+function heartb() {
+    var popup = document.getElementById("heartb");
+    popup.classList.toggle("show");
+}
+
+function yeezus() {
+    var popup = document.getElementById("yeezus");
+    popup.classList.toggle("show");
+}
+
+function grad() {
+    var popup = document.getElementById("grad");
+    popup.classList.toggle("show");
+}
+
+function mbdtf() {
+    var popup = document.getElementById("mbdtf");
+    popup.classList.toggle("show");
+}
+
+function yeezys() {
+    var popup = document.getElementById("yeezys");
+    popup.classList.toggle("show");
+}
